de371d22ad1dc2582d4590ab13668817b25ec533
Natalia <nataliamp87@gmail.com>
2018-05-22 22:03:19 -0400

    texto agregado y funcionalidad boton

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,8 +5,22 @@ window.onload = function () {
 
     })
 
+    var btn = document.getElementById("button");
+    var txt = document.getElementById("paragraph");
 
 
+    btn.addEventListener("click", function(){
+        var txt = document.getElementById("paragraph");
+        txt.innerHTML = "<h2>Jesse Pinkman iba a morir</h2><p>es uno de esos casos en que el guión marca la muerte del personaje pero finalmente, por diversas cuestiones, los creadores, showrunners o productores deciden mantenerlo con vida y, por lo tanto, ganando cada vez mayor protagonismo.</p>";
+        
+       })
+
+        
+       
+       
+
+
+    
 
 
 }
