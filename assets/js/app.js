31f16b9a43aa17d659d2ea88c08082e415c90fc0
Natalia <nataliamp87@gmail.com>
2018-05-22 23:25:13 -0400

    agrege imagen a texto

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,20 +7,20 @@ window.onload = function () {
 
     var btn = document.getElementById("button");
     var txt = document.getElementById("paragraph");
-
+     var img = document.getElementById("imgJesse");
+   
 
     btn.addEventListener("click", function(){
         var txt = document.getElementById("paragraph");
-        txt.innerHTML = "<h2>Jesse Pinkman iba a morir</h2><p>es uno de esos casos en que el guión marca la muerte del personaje pero finalmente, por diversas cuestiones, los creadores, showrunners o productores deciden mantenerlo con vida y, por lo tanto, ganando cada vez mayor protagonismo.</p>";
-        
-       })
-
-        
+        txt.innerHTML = "<h2>Jesse Pinkman iba a morir</h2><p>Es uno de esos casos en que el guión marca la muerte del personaje pero finalmente, por diversas cuestiones, los creadores, showrunners o productores deciden mantenerlo con vida y, por lo tanto, ganando cada vez mayor protagonismo.</p>";
+     
        
+        
+        
+    })
+      
        
 
-
-    
-
+        
 
 }
